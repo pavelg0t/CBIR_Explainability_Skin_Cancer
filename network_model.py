7b9c0f49ba318779c6de4132164f8c40b87e92f2
pavel.gotovski <pavel.gotovski@beyondanalysis.net>
2022-02-20 13:05:09 +0200

    check transform_data() worthiness v2

--- network_model.py
+++ network_model.py
@@ -84,12 +84,8 @@ def apply_model(train_size, data, captions, n_classes, wordtoix,
                              triplet,LAMBDA,
                              two_headed,two_head_size,is_training):
 
-    print('before: ',type(data))
-
-    data = tf.cond(is_training, lambda: da.transform_data(data, True, dim=image_size), 
-                                lambda: da.transform_data(data, False, dim=image_size))
-    print('after: ',type(data))
-
+    # data = tf.cond(is_training, lambda: da.transform_data(data, True, dim=image_size), 
+    #                             lambda: da.transform_data(data, False, dim=image_size))
 
     batch_size = tf.shape(data)[0]
 
